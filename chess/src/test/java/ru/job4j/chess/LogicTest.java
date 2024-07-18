2725db04e575e7a9080ce79488d27810cca95767
Иван <ivan1990kalinin@yandex.ru>
2024-07-18 21:19:44 +0700

    3. Каркас шахматной доски [#792]

---
+++ chess/src/test/java/ru/job4j/chess/LogicTest.java
@@ -0,0 +1,32 @@
+package ru.job4j.chess;
+
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.Test;
+import ru.job4j.chess.firuges.Cell;
+import ru.job4j.chess.firuges.Figure;
+import ru.job4j.chess.firuges.black.BishopBlack;
+
+import static org.assertj.core.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+public class LogicTest {
+
+    @Test
+    public void whenMoveThenFigureNotFoundException()
+            throws FigureNotFoundException, OccupiedCellException, ImpossibleMoveException {
+        Logic logic = new Logic();
+        FigureNotFoundException exception = assertThrows(FigureNotFoundException.class, () -> {
+            logic.move(Cell.C1, Cell.H6);
+        });
+        assertThat(exception.getMessage()).isEqualTo("Figure not found on the board.");
+    }
+
+    @Test
+    void whenMoveThenCellSteps() throws ImpossibleMoveException {
+        Figure figure = new BishopBlack(Cell.C1);
+        Cell[] steps = figure.way(Cell.F4);
+        Cell[] expected = {Cell.D2, Cell.E3, Cell.F4};
+        assertThat(expected).isEqualTo(steps);
+    }
+
+}
---
+++ chess/src/test/java/ru/job4j/chess/firuges/black/BishopBlackTest.java
@@ -0,0 +1,46 @@
+package ru.job4j.chess.firuges.black;
+import static org.assertj.core.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+import org.junit.jupiter.api.Test;
+import ru.job4j.chess.ImpossibleMoveException;
+import ru.job4j.chess.firuges.Cell;
+import ru.job4j.chess.firuges.Figure;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class BishopBlackTest {
+
+    @Test
+    void whenPositionThenReturnPosition() {
+        BishopBlack bishopBlack = new BishopBlack(Cell.C1);
+        Cell expected = bishopBlack.position();
+        assertThat(Cell.C1).isEqualTo(expected);
+    }
+
+    @Test
+    void whenCopyThenNewBishopBlack() {
+        BishopBlack bishopBlack = new BishopBlack(Cell.C1);
+        Figure newBishop = bishopBlack.copy(Cell.A2);
+        Cell expected = newBishop.position();
+        assertThat(expected).isEqualTo(Cell.A2);
+    }
+
+    @Test
+    void whenWayThenCorrect() {
+        BishopBlack bishopBlack = new BishopBlack(Cell.C1);
+        Cell[] actual = bishopBlack.way(Cell.G5);
+        Cell[] expected = {Cell.D2, Cell.E3, Cell.F4, Cell.G5};
+        assertThat(expected).isEqualTo(actual);
+
+    }
+
+    @Test
+    void whenWayIsNotDiagonal() {
+        BishopBlack bishopBlack = new BishopBlack(Cell.C1);
+        ImpossibleMoveException exception = assertThrows(
+                ImpossibleMoveException.class, () -> bishopBlack.way(Cell.C2));
+        assertThat(exception.getMessage()).isEqualTo("Could not way by diagonal from C1 to C2");
+    }
+
+}
