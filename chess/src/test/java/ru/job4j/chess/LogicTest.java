f08910cd83d0e1d2e22a6a98c1839e85a00232cc
Иван <ivan1990kalinin@yandex.ru>
2024-07-20 13:44:02 +0700

    3. Каркас шахматной доски [#792 #505750]

--- chess/src/test/java/ru/job4j/chess/LogicTest.java
+++ chess/src/test/java/ru/job4j/chess/LogicTest.java
@@ -4,9 +4,11 @@ import org.junit.jupiter.api.Test;
 import ru.job4j.chess.firuges.Cell;
 import ru.job4j.chess.firuges.Figure;
 import ru.job4j.chess.firuges.black.BishopBlack;
+import ru.job4j.chess.firuges.black.RookBlack;
 
 import static org.assertj.core.api.Assertions.*;
 import static org.junit.jupiter.api.Assertions.assertThrows;
+import static ru.job4j.chess.firuges.Cell.*;
 
 public class LogicTest {
 
@@ -14,10 +16,26 @@ public class LogicTest {
     public void whenMoveThenFigureNotFoundException()
             throws FigureNotFoundException, OccupiedCellException, ImpossibleMoveException {
         Logic logic = new Logic();
-        FigureNotFoundException exception = assertThrows(FigureNotFoundException.class, () -> {
+        FigureNotFoundException figureNotFoundException =
+                assertThrows(FigureNotFoundException.class, () -> {
             logic.move(Cell.C1, Cell.H6);
         });
-        assertThat(exception.getMessage()).isEqualTo("Figure not found on the board.");
+        BishopBlack bishopBlack = new BishopBlack(Cell.F8);
+        Figure rook = new RookBlack(Cell.E7);
+        logic.add(bishopBlack);
+        logic.add(rook);
+        OccupiedCellException occupiedCellException =
+                 assertThrows(OccupiedCellException.class, () -> {
+            logic.move(Cell.F8, Cell.E7);
+        });
+        assertThat(occupiedCellException.getMessage()).
+                isEqualTo("Cell E7 is occupied");
+        ImpossibleMoveException impossibleMoveException =
+                assertThrows(ImpossibleMoveException.class, () -> {
+            logic.move(Cell.F8, Cell.D8);
+        });
+        assertThat(impossibleMoveException.getMessage()).
+                isEqualTo("Could not way by diagonal from F8 to D8");
     }
 
     @Test
